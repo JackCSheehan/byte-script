8f5212278dbbc9d6b89b6648fcfdda26d3b80c71
jackcsheehan <jackcsheehan@users.noreply.github.com>
2020-06-10 00:17:48 -0500

    Implemented preprocessing and interpreting cmd commands

--- byte-script/byte-script/main.cpp
+++ byte-script/byte-script/main.cpp
@@ -4,13 +4,6 @@
 #include "InstructionUtils.h"
 #include "Interpreter.h"
 
-const std::string PREPROCESS_FLAG = "-p"; //Flag to only preprocess
-const std::string INTERPRET_FLAG = "-i";  //Flag to only interpret
-const std::string FULL_FLAG = "-pi";      //Flag to preprocess and interpret
-
-const int FLAG_INDEX = 1;                 //Index of flag in cmd arguments
-const int FILE_INDEX = 2;                 //Index of file in cmd arguments
-
 /*
 To preprocess:                ./byte-script -p [filename.bss]
 To interpret:                 ./byte-script -i [filename.bse]
@@ -18,8 +11,15 @@ To preprocess and interpret:  ./byte-script -pi [filename.bss]
 */
 int main(int argc, char** argv)
 {
-   std::string flag;       //The flag passed in cmd argument
-   std::string fileName;   //Name of file passed in cmd argument
+   const std::string PREPROCESS_FLAG = "-p"; //Flag to only preprocess
+   const std::string INTERPRET_FLAG = "-i";  //Flag to only interpret
+   const std::string FULL_FLAG = "-pi";      //Flag to preprocess and interpret
+
+   const int FLAG_INDEX = 1;                 //Index of flag in cmd arguments
+   const int FILE_INDEX = 2;                 //Index of file in cmd arguments
+
+   std::string flag;                         //The flag passed in cmd argument
+   std::string fileName;                     //Name of file passed in cmd argument
 
    //Ensure that there are enough arguments
    if (argc < 3)
@@ -32,21 +32,55 @@ int main(int argc, char** argv)
    fileName = argv[FILE_INDEX];
 
    //Check flag
-   if (argv[FLAG_INDEX] != PREPROCESS_FLAG || argv[FLAG_INDEX] != INTERPRET_FLAG || argv[FLAG_INDEX] != FULL_FLAG)
+   if (flag != PREPROCESS_FLAG && flag != INTERPRET_FLAG && flag != FULL_FLAG)
    {
-      std::cerr << "The flag " << argv[FLAG_INDEX] << " is not a valid flag. ";
+      std::cerr << "The flag " << flag << " is not a valid flag. ";
       std::cerr << "Please only use one of the following flags: " << PREPROCESS_FLAG << ", " << INTERPRET_FLAG << ", " << FULL_FLAG << ".\n";
    }
 
+   //TODO: Put code in if and else into functions in helper class to reduce duplicate code when -pi flag needs to be implemented
    //Process given file based on the flag
-   if (argv[FLAG_INDEX] == PREPROCESS_FLAG)
+   if (flag == PREPROCESS_FLAG)
    {
       //Use given source file as the source and grab the name of the given file as the name of the executable
       try
       {
-         Preprocessor preprocessor(argv[FILE_INDEX], );
+         //Get name of executable to make based on the name given by the user
+         std::string executableName = fileName.substr(0, fileName.find_last_of("."));
+
+         //Create preprocessor and preprocess given file
+         Preprocessor preprocessor(fileName, executableName);
+         preprocessor.process();
+         preprocessor.finish();
+
       }
-      
+      catch (ErrorOpeningSourceException e)
+      {
+         std::cerr << e.what() << "\n";
+      }
+      catch (ErrorOpeningExecutableException e)
+      {
+         std::cerr << e.what() << "\n";
+      }
+   }
+   else if (flag == INTERPRET_FLAG)
+   {
+      //Use given executable file and run it through the interpreter
+      try
+      {
+         //Create interpreter object and interpret
+         Interpreter interpreter(fileName);
+         interpreter.interpret();
+      }
+      //TODO: Change these catches to catch errors from the interpreter
+      catch (ErrorOpeningSourceException e)
+      {
+         std::cerr << e.what() << "\n";
+      }
+      catch (ErrorOpeningExecutableException e)
+      {
+         std::cerr << e.what() << "\n";
+      }
    }
 
    /*
